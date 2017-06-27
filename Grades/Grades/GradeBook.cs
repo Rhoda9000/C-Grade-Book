2e51c93d01ff96e95af595c8b9f11c2ba9eef5dc
Rhoda9000 <dylanrhoda@aim.com>
2017-06-27 19:12:07 +0200

    Add files via upload

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        private int[,] grades;
+
+        public GradeBook(int[,] gradesArray)
+        {
+            grades = gradesArray;
+        }
+
+
+        public void ProcessGrades()
+        {
+            OutputGrades();
+            Console.WriteLine("\n{0} {1}\n{2} {3}\n", "Lowest grade is", GetMin(), "Highest grade is", GetMax());
+            OutputBarChart();
+        }
+
+        public int GetMin()
+        {
+            int lowGrade = grades[0, 0];
+            foreach (int grade in grades)
+            {
+                if (grade < lowGrade)
+                    lowGrade = grade;
+            }
+            return lowGrade;
+        }
+
+        public int GetMax()
+        {
+            int highGrade = grades[0, 0];
+
+            foreach (int grade in grades)
+            {
+                if (grade > highGrade)
+                    highGrade = grade;
+            }
+            return highGrade;
+        }
+
+        public double GetAverage(int student)
+        {
+            int amount = grades.GetLength(1);
+            int total = 0;
+
+            for (int exam = 0; exam < amount; ++exam)
+
+                total += grades[student, exam];
+
+            return (double)total / amount;
+
+        }
+
+
+        public String letter(int gradeletter)
+        {
+            int amount = grades.GetLength(1);
+            int total = 0;
+
+            for (int test = 0; test < amount; ++test)
+
+                total += grades[gradeletter, test];
+            String letterGrade = "";
+            if (((double)total / amount >= 90))
+            {
+                letterGrade = "A";
+            }
+            else if (((double)total / amount >= 80))
+            {
+                letterGrade = "B";
+            }
+            else if (((double)total / amount >= 70))
+            {
+                letterGrade = "C";
+            }
+            else if (((double)total / amount >= 60))
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+
+            return letterGrade;
+        }
+
+
+        public void OutputBarChart()
+        {
+            Console.WriteLine("Overall grade distribution:");
+            int[] frequency = new int[11];
+
+            foreach (int grade in grades)
+            {
+                ++frequency[grade / 10];
+            }
+
+            for (int count = 0; count < frequency.Length; ++count)
+            {
+                if (count == 10)
+                    Console.Write(" 100: ");
+                else
+                    Console.Write("{0:D2}-{1:D2}: ", count * 10, count * 10 + 9);
+
+                for (int stars = 0; stars < frequency[count]; ++stars)
+
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+        }
+
+
+        public void OutputGrades()
+        {
+            Console.WriteLine("Welcome To The Grade Results:\n");
+            Console.Write("              ");
+
+            for (int test = 0; test < grades.GetLength(1); ++test)
+
+                Console.Write("Test {0}  ", test + 1);
+            Console.WriteLine("Average");
+            for (int student = 0; student < grades.GetLength(0); ++student)
+            {
+                Console.Write("Student {0,2}", student + 1);
+                for (int grade = 0; grade < grades.GetLength(1); ++grade)
+
+                    Console.Write("{0,8}", grades[student, grade]);
+                Console.WriteLine("  {0,9:F}", GetAverage(student) + " " + letter(student));
+
+            }
+        }
+    }
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBookTest
+    {
+        public static void getGrades(int[,] grades)
+        {
+            for (int student = 0; student < 10; student++)
+            {
+                Console.Write("Enter 5 grades for student ");
+                Console.WriteLine(student + 1);
+                for (int grade = 0; grade < 5; grade++)
+                {
+                    grades[student, grade] = int.Parse(Console.ReadLine());
+
+                    if ((grades[student, grade] < 0) || (grades[student, grade] > 100))
+                        Console.WriteLine("Invalid number ,try again", grade--);
+                }
+            }
+            Console.WriteLine("Thank you.");
+        }
+        static void Main(string[] args)
+        {
+
+            int[,] grades = new int[10, 5];
+            getGrades(grades);
+
+
+            int[,] gradesArray = { { grades[0,0], grades[0,1], grades[0,2],grades[0,3],grades[0,4] },
+                                   { grades[1,0], grades[1,1], grades[1,2],grades[1,3],grades[1,4] },
+                                   { grades[2,0], grades[2,1], grades[2,2],grades[2,3],grades[2,4] },
+                                   { grades[3,0], grades[3,1], grades[3,2],grades[3,3],grades[3,4] },
+                                   { grades[4,0], grades[4,1], grades[4,2],grades[4,3],grades[4,4] },
+                                   { grades[5,0], grades[5,1], grades[5,2],grades[5,3],grades[5,4] },
+                                   { grades[6,0], grades[6,1], grades[6,2],grades[6,3],grades[6,4] },
+                                   { grades[7,0], grades[7,1], grades[7,2],grades[7,3],grades[7,4] },
+                                   { grades[8,0], grades[8,1], grades[8,2],grades[8,3],grades[8,4] },
+                                   { grades[9,0], grades[9,1], grades[9,2],grades[9,3],grades[9,4] } };
+            GradeBook myGradeBook = new GradeBook(gradesArray);
+            myGradeBook.ProcessGrades();
+
+        }
+    }
+}
